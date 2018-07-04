942c8fc20ee863e3f515ea66fd96825496a11eea
root <root@lab7m21ll.uspeachsi.uspnet>
2018-07-04 18:12:21 -0300

    adicionado novas funcoes

--- ocd_ep2.java
+++ ocd_ep2.java
@@ -2,80 +2,78 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
-public class EP2 {
+public class ocd_ep2 {
     static void compilaCode(FileReader read) throws IOException {
         BufferedReader leitor = new BufferedReader(read);
         String line;
-        String subString = "";
+        String instrucao = "";
         String opCode;
         int i = 0;
-        while ((line = leitor.readLine()) != null) {
-            while (line.charAt(i) != ' ')  // separa o primeiro comando
-                subString += line.charAt(i);
 
-            switch (subString) {
+        while ((line = leitor.readLine()) != null) {
+            String[] parts = line.split(" ");
+            String instrucao = parts[0];
+            String[] c = parts[1].split(",");
+            String c1 = c[0];
+            String c2 = c[1];
+            
+            System.out.println(instrucao + " " + c1 + " " + c2);
+        }
+
+        // while ((line = leitor.readLine()) != null) {
+        //     while (line.charAt(i) != ' ')  // separa o primeiro comando
+        //         instrucao += line.charAt(i);
+
+            switch (instrucao) {
                 case "ADD":
                         opCode = "0001";
-
                         break;
                     case "MOV":
                         opCode = "0010";
-
                         break;
                     case "MUL":
                         opCode = "0011";
-
                         break;
                     case "DIV":
                         opCode = "0100";
-
                         break;
                     case "CMP":
                         opCode = "0101";
-
                         break;
                     case "INC":
                         opCode = "0110";
-
                         break;
                     case "JE":
                         opCode = "0111";
-
                         break;
                     case "JNE":
                         opCode = "1000";
-
                         break;
                     case "JG":
                         opCode = "1001";
-
                         break;
                     case "JGE":
                         opCode = "1010";
-
                         break;
                     case "JL":
                         opCode = "1011";
-
                         break;
                     case "JLE":
                         opCode = "1100";
-
                         break;
-                }
-            while (line.charAt(i) != ' ')
-                
             }
-        }
+        //     while (line.charAt(i) != ' ') {
+
+        //     }    
+        // }
+    }
 
 
     public static void main(String[] args) throws IOException {
-        List<M> lista = new LinkedList<M>(); // lista ligada com os comandos
+        //List<M> lista = new LinkedList<M>(); // lista ligada com os comandos
 
-        FileReader arquivo = new FileReader(args[0]);
-
-        if (arquivo != null) {
-            compilaCode(arquivo);
+        if (args[0] != null) {
+            compilaCode(new FileReader(args[0]));
         }
 
     }
