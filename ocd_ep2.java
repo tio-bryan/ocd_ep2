8b4a655641f9917e0ad5148b855dd8846f3b1e25
tio-bryan <bryan.pesca@hotmail.com>
2018-07-06 23:57:49 -0300

    novas funcoes adicionadas

--- ocd_ep2.java
+++ ocd_ep2.java
@@ -1,10 +1,13 @@
 import java.io.*;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
 
 public class ocd_ep2 {
+    static int AX, BX, CX, DX;
+
     // Baseado no http://www.guj.com.br/t/transforma-decimal-em-binario/47061
-    public static String DecimalParaBinario(int c2, int length) {
+    public static String DecimalParaBinario(int c2) {
 
         int d = c2;
         StringBuffer binario = new StringBuffer(); // guarda os dados
@@ -15,10 +18,10 @@ public class ocd_ep2 {
             d = d >> 1; // é a divisão que você deseja
         }
 
-        int length1 = length - binario.length();
+        int length = 8 - binario.length();
         
-        for (int i = 0; i < length1; i++) {
-            if (length1 - i == 1) {
+        for (int i = 0; i < length; i++) {
+            if (length - i == 1) {
                 binario.append(1);
                 break;
             }
@@ -28,10 +31,21 @@ public class ocd_ep2 {
         return binario.reverse().toString(); // inverte a ordem e imprime
     }
 
+    public static int BinarioParaDecimal(int binario) {
+        
+        String myString = new StringBuilder(Integer.toString(binario)).reverse().toString();
+        int result = 0;
+
+        for (int i = 0; i < myString.length(); i++) {
+            result += Character.getNumericValue(myString.charAt(i)) * Math.pow(2, i);
+        }
+
+        return result;
+    }
+
     static void compilaCode(FileReader read, List<M> fila) throws IOException {
         BufferedReader leitor = new BufferedReader(read);
         String line;
-        int length; // tamanho de parte do comando com inteiro
         int enderecoMEM = 0;// endereco do comando
 
         String instrucao = "";
@@ -121,8 +135,7 @@ public class ocd_ep2 {
                 }
 
                 try {
-                    length = 8;
-                    instrucao_bin += DecimalParaBinario(Integer.parseInt(c2), length); // caso o resto da instrucao seja um int
+                    instrucao_bin += DecimalParaBinario(Integer.parseInt(c2)); // caso o resto da instrucao seja um int
                 }
                 catch (Exception e) { // caso seja um registrador
                     switch (c2) {
@@ -156,8 +169,7 @@ public class ocd_ep2 {
             } else {
                 c1 = parts[1];
                 try {
-                    length = 0;
-                    instrucao_bin += DecimalParaBinario(Integer.parseInt(c1), length); // caso o resto da instrucao seja um int
+                    instrucao_bin += DecimalParaBinario(Integer.parseInt(c1)); // caso o resto da instrucao seja um int
                 } catch (Exception e) { // caso seja um registrador
                     switch (c1) {
                         case "AX":
@@ -187,44 +199,160 @@ public class ocd_ep2 {
                     }
                 }
             }
-            System.out.println(instrucao_bin.length());
             System.out.println(instrucao_bin);
             M objeto = new M(enderecoMEM, instrucao_bin);
             fila.add(objeto);
             enderecoMEM++;
         }
     }
+static void inc ()n int A{}
+        add(A, 1);
+    
+    static void add(int A, int B) {
+        ocd_ep2.AX = A + B;
+    }
+    static void sub(int A, int B){
+        ocd_ep2.AX = A - B; 
+    }
+    static void mul(int A, int B){
+        ocd_ep2.AX = A * B; 
+    }
+    static void div(int A, int B){
+        ocd_ep2.AX = A/B; 
+    }
+    static void cmp(int A, int B){
+        sub(A,B); //verifica conta passada if==0 entao eh igual
+    }
 
-    static void leCodigo(List<M> fila){
-        String bin = fila.get(0).opCode;
-        String instru = bin.substring(0,3);
 
-        switch (instru){
+    static void leCodigo(String code, int [] flags) {
+        
+        String instru = code.substring(0,3);
+        String a = code.substring(4,7);
+        String b = code.substring(9,15);
+        int reg1 = 0;
+        int reg2 = 0;
+
+        switch (a) {
+            case "0001":
+                reg1 = ocd_ep2.AX;
+                flags[5] = 1;  //porta de entrada do reg do diagrama - 1
+                break;
+            case "0010":
+                reg1 = ocd_ep2.BX;
+                flags[7] = 1;
+                break;
+            case "0011":
+                reg1 = ocd_ep2.CX;
+                flags[9] = 1;
+                break;
+            case "0100":
+                reg1 = ocd_ep2.DX; //portas de DX entram entre a seq do diagrama
+                flags[11] = 1;
+                break;
+            case "0101":
+                //instrucao_bin += "0101";
+                break;
+            case "0110":
+                //instrucao_bin += "0110";
+                break;
+            case "0111":
+                //instrucao_bin += "0111";
+                break;
+            case "1000":
+                //instrucao_bin += "1000";
+                break;
+        }
+
+        if (code.charAt(8) == '1') {
+            reg2 = BinarioParaDecimal(Integer.parseInt(b));
+        } else {
+            switch (b) {
+            case "0001":
+                reg2 = ocd_ep2.AX;
+                flags[6] = 1;  //porta de saida do reg do diagrama - 1
+                break;
+            case "0010":
+                reg2 = ocd_ep2.BX;
+                flags[8] = 1;
+                break;
+            case "0011":
+                reg2 = ocd_ep2.CX;
+                flags[10] = 1;
+                break;
+            case "0100":
+                reg2 = ocd_ep2.DX; 
+                flags[12] = 1;
+                break;
+            case "0101":
+                //instrucao_bin += "0101";
+                break;
+            case "0110":
+                //instrucao_bin += "0110";
+                break;
+            case "0111":
+                //instrucao_bin += "0111";
+                break;
+            case "1000":
+                //instrucao_bin += "1000";
+                break;
+        }
+        }                    
+
+    
+        switch (instru) {
             case "0001":
             case "0010":
+                inc(reg1);
             case "0011":
+                add(reg1, reg2);
+                break;
             case "0100":
+                sub(reg1, reg2);
             case "0101":
+                mul(reg1, reg2);
             case "0110":
+                div(reg1, reg2);
             case "0111":
+                cmp(reg1, reg2);
             case "1000":
             case "1001":
             case "1010":
             case "1011":
             case "1100":
             case "1101":
-
         }
+    }
+
+    static String busca(List<M> lista, int PC) {
+        Iterator it = lista.iterator();
+
+        while (it.hasNext()) {
+            M MAR = lista.get(PC);
+            if (MAR.endereco == PC) {
+                PC++;
+                return MAR.opCode;
+            }
+        }
+
+        return "-1";
+    }
+    static void registradores() { // Printa os registradores apos ciclo
+
+    }
+    void verificaFlag(int [] flags){
+        
     }
 
     public static void main(String[] args) throws IOException {
         List<M> fila = new LinkedList<M>(); // lista ligada com o binario e endereco
-        int[] barramento = new int[13]; // barramento 
+        int[] flags = new int[24]; // barramento 
 
         if (args[0] != null) {
             compilaCode(new FileReader(args[0]), fila);
         }
 
+        leCodigo(busca(fila, 0), flags);
 
     }
 }
